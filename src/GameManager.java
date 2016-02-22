bb04f9588a4389b4ded2974d697ab055d99fb568
Shinyoung <fairesy@gmail.com>
2016-02-22 18:26:32 +0900

    multiple player abled. 10th frame strike case & lazy printer left to do

--- src/GameManager.java
+++ src/GameManager.java
@@ -24,71 +24,72 @@ public class GameManager {
 		ScoreCalculator calculator = new ScoreCalculator();
 		
 		for(int frameId=0; frameId<10; frameId++){
-			System.out.println((frameId+1) + "번째 프레임입니다.");
-			//TODO 여러명 플레이어 가능하게. -> scoreboardList.get(0)부분.
-			Scoreboard currentScoreboard = this.scoreboardList.get(0);
-			Frame currentFrame = currentScoreboard.frameList.get(frameId);
-			
-			
-			System.out.println("첫 번째 넘어뜨린 핀 수는?");
-			int firstRoll = roll.nextInt();
-			//넘어뜨린 핀 수는 leftPins보다 많을 수 없다.
-			if(firstRoll > currentFrame.leftPins){
-				//throw exception.
-			}
-			
-			currentFrame.firstRoll = firstRoll;
-			currentFrame.leftPins = currentFrame.leftPins - firstRoll;
-			
-			String currentFrameState = checker.checkStrike(currentFrame);
-			
-			if(currentFrameState != "strike"){
-				System.out.println("두 번째 넘어뜨린 핀 수는?");
-				int secondRoll = roll.nextInt();
+			for(int boardId = 0; boardId < this.scoreboardList.size(); boardId++){
+				System.out.println((boardId+1) + "번째 플레이어, " + (frameId+1) + "번째 프레임입니다.");
+				Scoreboard currentScoreboard = this.scoreboardList.get(boardId);
+				Frame currentFrame = currentScoreboard.frameList.get(frameId);
 				
-				if(secondRoll > currentFrame.leftPins){
+				
+				System.out.println("첫 번째 넘어뜨린 핀 수는?");
+				int firstRoll = roll.nextInt();
+				//넘어뜨린 핀 수는 leftPins보다 많을 수 없다.
+				if(firstRoll > currentFrame.leftPins){
 					//throw exception.
 				}
 				
-				currentFrame.secondRoll = secondRoll;
-				currentFrame.leftPins = currentFrame.leftPins - firstRoll;
-				
-				currentFrameState = checker.checkSpare(currentFrame);
-			}
-			
-			//10번째 프레임이 스트라이크/스페어일 경우 보너스 프레임까지 플레이.
-			String tenthState = currentFrame.state;
-			if((frameId == 9) && (tenthState == "spare")){
-				System.out.println((frameId+1) + "보너스 프레임입니다.");
-				currentScoreboard = this.scoreboardList.get(0);
-				currentFrame = currentScoreboard.frameList.get(frameId+1);
-				
-				System.out.println("첫 번째 보너스 롤");
-				firstRoll = roll.nextInt();
-				currentFrame.firstRoll = firstRoll;
-				currentFrame.leftPins = currentFrame.leftPins - firstRoll;
-			}
-			
-			if((frameId == 9) && (tenthState == "strike")){
-				System.out.println((frameId+1) + "보너스 프레임입니다.");
-				currentScoreboard = this.scoreboardList.get(0);
-				currentFrame = currentScoreboard.frameList.get(frameId+1);
-				
-				System.out.println("첫 번째 보너스 롤");
-				firstRoll = roll.nextInt();
 				currentFrame.firstRoll = firstRoll;
 				currentFrame.leftPins = currentFrame.leftPins - firstRoll;
 				
-				System.out.println("두 번째 보너스 롤");
-				int secondRoll = roll.nextInt();
-				currentFrame.secondRoll = secondRoll;
-				currentFrame.leftPins = currentFrame.leftPins - secondRoll;
+				String currentFrameState = checker.checkStrike(currentFrame);
+				
+				if(currentFrameState != "strike"){
+					System.out.println("두 번째 넘어뜨린 핀 수는?");
+					int secondRoll = roll.nextInt();
+					
+					if(secondRoll > currentFrame.leftPins){
+						//throw exception.
+					}
+					
+					currentFrame.secondRoll = secondRoll;
+					currentFrame.leftPins = currentFrame.leftPins - firstRoll;
+					
+					currentFrameState = checker.checkSpare(currentFrame);
+				}
+				
+				//10번째 프레임이 스트라이크/스페어일 경우 보너스 프레임까지 플레이.
+				String tenthState = currentFrame.state;
+				if((frameId == 9) && (tenthState == "spare")){
+					System.out.println((frameId+1) + "보너스 프레임입니다.");
+					currentScoreboard = this.scoreboardList.get(boardId);
+					currentFrame = currentScoreboard.frameList.get(frameId+1);
+					
+					System.out.println("첫 번째 보너스 롤");
+					firstRoll = roll.nextInt();
+					currentFrame.firstRoll = firstRoll;
+					currentFrame.leftPins = currentFrame.leftPins - firstRoll;
+				}
+				
+				if((frameId == 9) && (tenthState == "strike")){
+					System.out.println((frameId+1) + "보너스 프레임입니다.");
+					currentScoreboard = this.scoreboardList.get(boardId);
+					currentFrame = currentScoreboard.frameList.get(frameId+1);
+					
+					System.out.println("첫 번째 보너스 롤");
+					firstRoll = roll.nextInt();
+					currentFrame.firstRoll = firstRoll;
+					currentFrame.leftPins = currentFrame.leftPins - firstRoll;
+					
+					System.out.println("두 번째 보너스 롤");
+					int secondRoll = roll.nextInt();
+					currentFrame.secondRoll = secondRoll;
+					currentFrame.leftPins = currentFrame.leftPins - secondRoll;
+				}
+				
+				calculator.calculateEachFrameTotalUntil(frameId, currentScoreboard);
+				calculator.calculatePlayerTotal(currentScoreboard);
+				this.scoreboardPrinter.printCurrentScore();
+				
 			}
-			
-			calculator.calculateEachFrameTotalUntil(frameId, currentScoreboard);
-			calculator.calculatePlayerTotal(currentScoreboard);
-			this.scoreboardPrinter.printCurrentScore();
-			
 		}
 	}
 }
